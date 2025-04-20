33a653e060ed863a05254af5eddc5897e3887c52
yurisdevops <yurimachado.dev@gmail.com>
2025-04-20 15:14:19 -0300

    Update app.route

---
+++ src/pages/FinishhOrder/index.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParamsList } from "../../routes/app.routes";
+import { api } from "../../services/api";
+
+type RouteDetailParams = {
+  FinishOrder: {
+    number: number | string;
+    order_id: string;
+  };
+};
+
+type FinishOrderRouteProp = RouteProp<RouteDetailParams, "FinishOrder">;
+export default function FinishOrder() {
+  const route = useRoute<FinishOrderRouteProp>();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<StackParamsList>>();
+  async function handleFinish() {
+    try {
+      await api.put("/order/send", {
+        order_id: route.params?.order_id,
+      });
+
+      navigation.popToTop();
+    } catch (error) {
+      console.error("Error ao tentar finalizar o pedido.", error);
+    }
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.alert}>Você deseja finalizar este pedido:</Text>
+      <Text style={styles.title}>Mesa {route.params?.number}</Text>
+      <TouchableOpacity style={styles.button} onPress={handleFinish}>
+        <Text style={styles.textButton}>Finalizar Pedido</Text>
+        <Feather name="shopping-cart" size={20} color={"#1d1d2e"} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1d1d2e",
+    paddingVertical: "5%",
+    paddingHorizontal: "4%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  alert: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 12,
+    color: "#fff",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 12,
+  },
+  button: {
+    backgroundColor: "#3fffa3",
+    flexDirection: "row",
+    width: "65%",
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+  },
+  textButton: {
+    fontSize: 18,
+    marginRight: 8,
+    fontWeight: "bold",
+    color: "#1d1d2e",
+  },
+});
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,97 @@
+import React, { useContext, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import { AuthContext } from "../../contexts/AuthContext";
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn, loadingAuth } = useContext(AuthContext);
+
+  async function handleLogin() {
+    if (!email || !password) return;
+    await signIn({ email, password });
+  }
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require("../../assets/logo.png")} />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu email..."
+          placeholderTextColor={"#F0F0F0"}
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Digit sua senha..."
+          placeholderTextColor={"#F0F0F0"}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          {loadingAuth ? (
+            <ActivityIndicator size={25} color={"#fff"} />
+          ) : (
+            <Text style={styles.buttonText}>Acessar</Text>
+          )}
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1d1d2e",
+  },
+  logo: {
+    width: 320,
+    resizeMode: "contain",
+    marginBottom: 18,
+  },
+  inputContainer: {
+    width: "95%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 34,
+    paddingHorizontal: 14,
+  },
+  input: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#101026",
+    marginBottom: 12,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+    color: "#fff",
+  },
+  button: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#3fffa3",
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#101026",
+  },
+});
